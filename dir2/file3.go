d5daf441cd199e214b6f0bdbcef770e0c58a45d5
TrKhoi <trKhoi>
2020-07-13 20:54:46 +0700

    .

--- dir2/file3.go
+++ dir2/file3.go
@@ -1,14 +1,5 @@
 package main
-<<<<<<< HEAD
-
-import (
-	"fmt"
-	"os"
-)
 
 func main() {
-	argName := os.Args[1]
-	fmt.Printf("the argument is %s", argName)
+
 }
-=======
->>>>>>> 2583a081ae26e25b3b2d3e496aa11cc66c0284b0
